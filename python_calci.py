7274f1a7a7bb7075ef79789b2914d4f67e3f23a5
chandan <chandrakanttiwari31@gmail.com>
2019-11-10 20:40:03 +0530

    while

--- python_calci.py
+++ python_calci.py
@@ -12,24 +12,26 @@ def val_frm_txt(text):
         except Exception:
             char.append(ch)
     return val,char
+input_yes_no="yes"
+while(input_yes_no == "yes"):
+    input_val = input("Enter what you want !")
+    val, operation = val_frm_txt(input_val)
+    for ch in operation:
+        if(ch.lower() in Sum):
+            add=0
+            for i in val:
+                add=add + i
+            print(add)
+            break
+        elif(ch.lower() in subtract):
+            sub = val[0]
+            del val[0]
+            for i in val:
+                sub = sub - i
+            print(sub)
+            break
+    else:
+        print(" Sorry we could not find what you want from these expressions ")
+    input_yes_no = input("Do you want continue::yes/no ")
 
-input_val=input("Enter what you want !")
-a,b=val_frm_txt(input_val)
-
-for ch in b:
-    if(ch.lower() in Sum):
-        add=0
-        for i in a:
-            add=add + i
-        print(add)
-        break
-    elif(ch.lower() in subtract):
-        sub = a[0]
-        a[0]=0
-        for i in a:
-            sub = sub - i
-        print(sub)
-        break
-else:
-    print("Sorry we could not find what you want from these expressions")
-
+print("!!!!!!!  Thanks for Using this Smart Calculator  !!!!!!!")
